7a6fd2b6187012b830cce17395e737f066f2c4ad
JuanBrachoDev <juanjobracho@gmail.com>
2021-03-06 00:51:33 +0000

    Added placeholder discussion template, added discussion style to style.css, added link to discussion in index.html, added mobile CDN to base.html

--- run.py
+++ run.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/discussion")
+def discussion():
+    return render_template("discussion.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
+    $('.dropdown-trigger').dropdown();
+});
 
-  });
+$(function(){
+  $( ".post" ).bind( "taphold", tapholdHandler);
+ 
+  function tapholdHandler( event ){
+    $( ".post-options-button" ).click();
+  }
+});
