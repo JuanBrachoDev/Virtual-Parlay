99952f58b1bb56a7cc67d205f8c08fcf06b4e652
JuanBrachoDev <juanjobracho@gmail.com>
2021-03-16 12:28:57 +0000

    Installed pytest and pytest-flask, updated requirements.txt, added basic py_test file.

---
+++ py_test.py
@@ -0,0 +1,19 @@
+import pytest
+from flask import Flask
+import run
+
+
+def test_home_page_post():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is is posted to (POST)
+    THEN check that a '405' status code is returned
+    """
+    flask_app = Flask('flask_test.cfg')
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/')
+        assert response.status_code == 405
+        assert b"Welcome to the Flask User Management Example!" not in response.data
+        
