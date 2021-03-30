f9e2d2f1a0721ec441b017b4feef0f0b66eb34fe
JuanBrachoDev <juanjobracho@gmail.com>
2021-03-30 07:42:57 +0000

    Fixed typos in discussion html, improved file upload validation for edit_profile.

---
+++ static/js/edit_profile.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {       
+    $('#profile_picture').bind('change', function() {
+        // Checks if file is bigger than 5MB, shows error message and clears input in case it is,
+        // otherwise it removes the error message if it was showing.
+        if(this.files[0].size > 5242880) {
+            $('#profile_picture').val("")
+            $("#helper_profile_picture").removeClass("hide")
+        } else {
+            $("#helper_profile_picture").addClass("hide")
+        };
+        
+    });
+});
